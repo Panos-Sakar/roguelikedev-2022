eff00688aec8434c46594539e05bdb12c1ea425a
Dodos626 <chlktheo@gmail.com>
2022-07-07 20:05:44 +0300

    engine entity class created

--- Flamingos/main.py
+++ Flamingos/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from input_handlers import EventHandler
-
+from entity import Entity
 ART_PATH = 'Assets/Art/'
 
 
@@ -11,13 +11,16 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
 
     tileset = tcod.tileset.load_tilesheet(
         ART_PATH + "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
     event_handler = EventHandler()
+    player = Entity(int(screen_width/2), int(screen_height/2), "@", (255, 123, 255))
+    npc = Entity(int(screen_width/2) - 5, int(screen_height/2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
             screen_width,
@@ -29,21 +32,9 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
 
         while True:
-
-            root_console.print(player_x, player_y, string="@")
-
-            context.present(root_console)
-
-            root_console.clear()
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-                if action is None:
-                    continue
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-                if isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events= tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
